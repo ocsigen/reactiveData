e4913cfdd3a6cb0d87d00cb1b4be9077c3dca134
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-05-19 13:16:43 +0200

    update builder
    custom builder
    
    merge

--- pkg/build.ml
+++ pkg/build.ml
@@ -2,8 +2,13 @@
 #directory "pkg";;
 #use "topkg.ml";;
 
+
+let builder = `Other
+    ( "ocamlbuild -use-ocamlfind -classic-display -plugin-tag \"package(js_of_ocaml.ocamlbuild)\"",
+      "_build")
+
 let () =
-  Pkg.describe "reactiveList" ~builder:`OCamlbuild [
+  Pkg.describe "reactiveList" ~builder [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "src/reactiveList";
     Pkg.lib ~exts:Exts.module_library "src/reactiveDomList" ]
